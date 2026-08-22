c54f989b00641f0482455dccd4e564e675de2316
Abhishek soni (Abhi) <iamabhi.as0001@gmail.com>
2026-08-22 23:47:48 +0530

    Time: 18 ms (46.25%), Space: 43.5 MB (79.66%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.java
@@ -0,0 +1,48 @@
+class Solution {
+    public String longestPalindrome(String s) {
+
+        if (s == null || s.length() < 2) {
+            return s;
+        }
+
+        int start = 0;
+        int maxLen = 1;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // Odd length palindrome
+            int left = i;
+            int right = i;
+
+            while (left >= 0 && right < s.length()
+                    && s.charAt(left) == s.charAt(right)) {
+
+                if (right - left + 1 > maxLen) {
+                    start = left;
+                    maxLen = right - left + 1;
+                }
+
+                left--;
+                right++;
+            }
+
+            // Even length palindrome
+            left = i;
+            right = i + 1;
+
+            while (left >= 0 && right < s.length()
+                    && s.charAt(left) == s.charAt(right)) {
+
+                if (right - left + 1 > maxLen) {
+                    start = left;
+                    maxLen = right - left + 1;
+                }
+
+                left--;
+                right++;
+            }
+        }
+
+        return s.substring(start, start + maxLen);
+    }
+}
